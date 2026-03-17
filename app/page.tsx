7373da5593da2af009e901244b45dea35228c157
parkBeomSeo <beam031761@gamil.com>
2026-03-17 19:44:26 +0900

    summary plus page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Summary from "@/components/Summary";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <section className="flex items-center justify-center min-h-[calc(100vh-87px)]">
           {/* 콘텐츠 */}
           <Hero />
+          <Summary />
         </section>
       </main>
     </div>
