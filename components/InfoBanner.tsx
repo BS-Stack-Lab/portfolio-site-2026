adc0a2040dbaac56d32d0998499561b6dceefc1a
parkBeomSeo <beam031761@gamil.com>
2026-03-18 01:05:25 +0900

    info banner img and title sujung

--- components/InfoBanner.tsx
+++ components/InfoBanner.tsx
@@ -13,22 +13,22 @@ export default function InfoBanner() {
       <div className="relative w-full max-w-[1280px] h-full mx-auto flex flex-col items-center">
         
         {/* 1. 소제목: 내 정보. */}
-        <span className="absolute top-[140px] font-wanted font-bold text-[24px] leading-[32px] tracking-[-0.23px] text-[#171717] text-center">
+        <span className="absolute top-[140px] font-wanted font-bold xl:text-[24px] xl:leading-[32px] md:text-[24px] md:leading-[32px] text-[16px] leading-[24px] tracking-[-0.23px] text-[#171717] text-center">
           내 정보.
         </span>
 
         {/* 2. 메인 타이틀: 더욱더 과감하게. 더욱더 세밀하게. */}
         <h2 className="absolute top-[204px] w-full max-w-[700px] font-wanted font-bold 
-          xl:text-[80px] xl:leading-[94px] md:text-[60px] md:leading-[72px] text-[40px] leading-[48px]
+          xl:text-[80px] xl:leading-[94px] md:text-[68px] md:leading-[80px] text-[48px] leading-[52px]
           text-center tracking-[0.23px]
           /* 그라데이션 텍스트 처리 */
           bg-gradient-to-r from-[#449EFF] via-[#A4D0FF] to-[#BDDDFF] bg-clip-text text-transparent">
-          더욱더 과감하게.<br className="md:hidden" /> 더욱더 세밀하게.
+          더욱더 과감하게.<br /> 더욱더 세밀하게.
         </h2>
 
         {/* 3. 설명 문구 */}
         <p className="absolute top-[424px] w-full max-w-[700px] px-[20px] font-wanted font-semibold 
-          xl:text-[20px] xl:leading-[28px] text-[16px] leading-[24px]
+          xl:text-[20px] xl:leading-[28px] mb:text-[20px] mb:leading-[28px] text-[16px] leading-[24px]
           text-center text-[#737373] tracking-[-0.12px] break-keep">
           새로운 가능성을 빌딩합니다. 비전공자의 시선으로 발견한 아이디어, 더 세밀해진 기획 그리고 구현을 향한 집요함으로 가치 있는 서비스를 설계합니다. 기획부터 디자인을 넘어 개발까지 이어지는 폭넓은 경험으로는 당신이 상상하는 모든 걸 더 많이 구현하고, 더 많이 보여줄 수 있죠. AI를 적극 활용하여 앞당긴 실행력 덕에 더 부드러워진 작업 흐름과 더 몰입감 넘치는 프로젝트 빌딩도 즐길 수 있습니다. 직접 그 진가를 확인해 보세요.
         </p>
@@ -37,7 +37,7 @@ export default function InfoBanner() {
         <div className="absolute top-[612px] w-full max-w-[905px] flex justify-center">
           <div className="relative w-full aspect-[905/946]">
             <img 
-              src="/asset/infoBanner/infoPhone.jpg" 
+              src="/asset/infoBanner/infoPhone.png" 
               alt="Phone Mockup"
               className="w-full h-full object-contain"
             />
