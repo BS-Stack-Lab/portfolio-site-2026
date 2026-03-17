3be7fa5cc782c1542f620f18497af5e52260756a
parkBeomSeo <beam031761@gamil.com>
2026-03-18 00:36:04 +0900

    hero arrow icon size sujung

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -60,7 +60,7 @@ export default function Hero() {
               <img 
                 src="/asset/hero/hero_arrow.gif" 
                 alt="arrow icon" 
-                className="w-20 h-20 object-contain scale-50" 
+                className="w-20 h-20 object-contain scale-60" 
               />
             </div>
           </button>
