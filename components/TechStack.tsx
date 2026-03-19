626831e6060dc63960c619176029cbbb6fdba6e3
parkBeomSeo <beam031761@gamil.com>
2026-03-19 10:14:15 +0900

    tech modal final modify

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -27,8 +27,9 @@ export default function TechStack() {
         <div className="md:w-[80%] w-full flex justify-start">
           <h2 className="font-wanted font-bold tracking-[0.23px] xl:text-[64px] xl:leading-[78px] md:text-[48px] md:leading-[60px] text-[32px] leading-[42px] max-w-[603px]
             bg-gradient-to-r from-[#449EFF] via-[#A4D0FF] to-[#BDDDFF] bg-clip-text text-transparent">
-            디자인과 로직의 조화. <br className="hidden md:block" />
-            감각적인 기획에 개발의 정교함을 더하다.
+            디자인과 로직의 조화. <br />
+            감각적인 기획에 개발의 <br />
+            정교함을 더하다.
           </h2>
         </div>
 
@@ -40,15 +41,15 @@ export default function TechStack() {
         </div>
 
         {/* 하단 텍스트 및 버튼 영역 */}
-        <div className="w-full flex flex-col items-center gap-[60px] md:gap-[80px]">
-          <div className="md:w-[80%] w-full flex flex-col md:flex-row justify-between items-start gap-[40px] md:gap-[60px] xl:gap-[88px]">
-            <p className="font-wanted font-semibold text-[#737373] xl:text-[20px] xl:leading-[28px] md:text-[18px] md:leading-[26px] text-[15px] leading-[1.6] md:flex-1 break-keep">
+        <div className="w-full flex flex-col items-center xl:gap-[40px] md:gap-[80px] gap-[40px]">
+          <div className="md:w-[80%] w-full flex flex-col md:flex-row justify-between items-start gap-[24px] md:gap-[40px] xl:gap-[80px]">
+            <p className="font-wanted font-semibold text-[#737373] xl:text-[20px] xl:leading-[28px] md:text-[18px] md:leading-[26px] text-[16px] leading-[1.6] md:flex-1 break-keep">
             내가 상상하는 모든 것을 실체로 만드는 원동력, 저의 풀스택 툴킷입니다. AI를 효율적인 파트너로 삼아 작업의 효율을 극대화하는 것은 물론, 보이지 않는 로직부터 사용자에게 닿는 감각적인 부분까지 모든 과정을 단단하게 빌딩하죠. 기획부터 디자인까지 직접 고민하며 쌓아온 저의 폭넓은 시야는 아이디어를 정교하게 설계하고, 구체화하여 세상에 내놓을 수 있게 해줍니다. 완벽한 결과물을 향한 집요함은 아주 작은 디테일까지 놓치지 않고 설계하여 사용자에게 결점 없는 경험을 선사하게 한답니다.
             </p>
 
             <div className="flex flex-col gap-[4px] md:w-auto xl:w-[343px]">
               <span className="font-wanted font-semibold text-[#A1A1A1] text-[14px]">최대</span>
-              <h3 className="font-wanted font-bold xl:text-[40px] xl:leading-[52px] md:text-[32px] text-[24px]
+              <h3 className="font-wanted font-bold xl:text-[40px] xl:leading-[52px] md:text-[32px] md:leading-[42px] text-[24px] leading-[3px]
                 bg-gradient-to-r from-[#449EFF] via-[#A4D0FF] to-[#BDDDFF] bg-clip-text text-transparent">
                 상상을 실체로 만드는 <br /> 압도적 가속도.
               </h3>
