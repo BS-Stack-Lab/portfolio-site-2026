b6e409d35c89cd017b40749298f73e92782d8286
parkBeomSeo <beam031761@gamil.com>
2026-03-19 12:08:25 +0900

    hero section final modify

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Header />
       {/* 87px 마진으로 블랙 바 영역 확보 */}
       <main className="xl:mt-[87px] min-h-[calc(100vh-87px)]">
-        <section className="flex flex-col items-center justify-center min-h-[calc(100vh-87px)]">
+        <section className="flex bg-white flex-col items-center justify-center min-h-[calc(100vh-87px)]">
           {/* 콘텐츠 */}
           <Hero />
           <Summary />
